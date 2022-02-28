83e67e3a8b041582dc06e9eee4dfb42a899b9058
TikvaYosef <tikvayosef13@gmail.com>
2022-02-28 18:43:04 +0200

    c

--- SERVER/controllers/employeeController.js
+++ SERVER/controllers/employeeController.js
@@ -6,23 +6,23 @@ const getAll = async (req, res) => {
         .catch((err) => res.status(404).send({ massege: err }));
 }
 
-const GETBYID = async (req, res) => {
-    await employees.find()
-        .then((id) => { if (res.id == id) res.send(res) })
-        .catch((err) => res.status(404).send({ massege: err }))
-}
+// const GETBYID = async (req, res) => {
+//     await employees.find()
+//         .then((id) => { if (res.id == id) res.send(res) })
+//         .catch((err) => res.status(404).send({ massege: err }))
+// }
 
-const POST =  (req, res) => {
-    // await employees.create(req.body)
-    //     .then((result) => res.send(result))
-    //     .catch((err) => res.status(404).send({ massege: err }));
+// const POST =  (req, res) => {
+//     // await employees.create(req.body)
+//     //     .then((result) => res.send(result))
+//     //     .catch((err) => res.status(404).send({ massege: err }));
 
-}
-const PUT = (req, res) => {
+// }
+// const PUT = (req, res) => {
 
-}
-const DELETE = (req, res) => {
+// }
+// const DELETE = (req, res) => {
 
-}
+// }
 
-module.exports = { getAll, GETBYID, POST, PUT, DELETE }
+module.exports = { getAll}
--- SERVER/routes/employeeRoter.js
+++ SERVER/routes/employeeRoter.js
@@ -4,12 +4,12 @@ const requestHeandler = require("../controllers/employeeController")
 
 router.get("/", requestHeandler.getAll)
 
-router.get("/:id", requestHeandler.GETBYID)
+// router.get("/:id", requestHeandler.GETBYID)
 
-router.post("/", requestHeandler.POST)
+// router.post("/", requestHeandler.POST)
 
-router.put("/:id",requestHeandler.PUT)
+// router.put("/:id",requestHeandler.PUT)
 
-router.delete("/:id",requestHeandler.DELETE)
+// router.delete("/:id",requestHeandler.DELETE)
 
 module.exports = router;
