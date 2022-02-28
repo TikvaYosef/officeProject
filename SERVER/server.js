1529581ca670233dc963e9e5a6a8e132431b4383
TikvaYosef <tikvayosef13@gmail.com>
2022-02-28 17:14:37 +0200

    commit

--- SERVER/server.js
+++ SERVER/server.js
@@ -6,7 +6,7 @@ const route = require("./routes/employeeRoter")
 const officeServer = express();
 officeServer.use(express.json())
 officeServer.use(cors());
-const PORT = 8080 || PORT
+const PORT =  process.env.PORT
 officeServer.listen(PORT);
 
 officeServer.get("/",(req,res)=>res.send("server is up"))
