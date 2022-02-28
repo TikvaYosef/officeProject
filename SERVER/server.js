98eedfa42371ffe0d0c9379d527e20fc4081f346
TikvaYosef <tikvayosef13@gmail.com>
2022-02-28 18:38:15 +0200

    commit

--- SERVER/server.js
+++ SERVER/server.js
@@ -2,13 +2,13 @@ require("dotenv").config()
 const express = require("express")
 require("./DB")
 const cors = require("cors");
-const route = require("./routes/employeeRoter")
+const routing = require("./routes/employeeRoter")
 const officeServer = express();
 officeServer.use(express.json())
 officeServer.use(cors());
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8080
 officeServer.listen(PORT);
 
 officeServer.get("/", (req, res) => res.send("server is up"))
-officeServer.use("/", route)
+officeServer.use("/", routing)
 
