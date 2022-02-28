47ca9714c93f3ed6a993ed67070dd00ae296a5c7
TikvaYosef <tikvayosef13@gmail.com>
2022-02-28 19:00:23 +0200

    vi

--- SERVER/server.js
+++ SERVER/server.js
@@ -6,9 +6,11 @@ const routing = require("./routes/employeeRoter")
 const officeServer = express();
 officeServer.use(express.json())
 officeServer.use(cors());
-const PORT = process.env.PORT || 8080
-officeServer.listen(PORT);
+// const PORT = process.env.PORT || 8080
+officeServer.listen(process.env.PORT,
+    () => console.log(`server is running on port ${process.env.PORT}`)
+);
 
 officeServer.get("/", (req, res) => res.send("server is up"))
-officeServer.use("/", routing)
+officeServer.use("/employees", routing)
 
