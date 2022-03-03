7a931b615096cdc82ec13b6d30ff1019bde433b2
TikvaYosef <tikvayosef13@gmail.com>
2022-03-03 13:44:12 +0200

    comm

--- client/src/services/employeeService.jsx
+++ client/src/services/employeeService.jsx
@@ -1,5 +1,5 @@
 
-const BASE_URL = process.env.NODE_ENV === "production" ? -
+const BASE_URL = process.env.NODE_ENV === "production" ? 
     "https://officemernapp.herokuapp.com/employees":
     "http://localhost:5050/employees";
 
